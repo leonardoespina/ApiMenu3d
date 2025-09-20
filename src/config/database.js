f23199ce13750a251216378d0ea5e3c026d59798
leonardoespina <leonardoespina@hotmail.com>
2025-09-20 09:15:38 -0400

    dialectModule: pg,

--- src/config/database.js
+++ src/config/database.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-
+const pg = require("pg");
 // Configuración anterior para MySQL
 // const sequelize = new Sequelize(
 //   process.env.DB_NAME,
@@ -16,6 +16,7 @@ const { Sequelize } = require("sequelize");
 // Nueva configuración para Supabase (PostgreSQL)
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: "postgres",
+  dialectModule: pg,
   protocol: "postgres",
   logging: false,
 });
